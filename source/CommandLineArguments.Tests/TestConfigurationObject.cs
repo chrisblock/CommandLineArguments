06ef1b48131b5673bd9b821fba86ab2eb5a5bee0
Chris Block <chris.block@gmail.com>
2011-12-13 22:10:36 -0600

    Deleted unused files.

--- source/CommandLineArguments.Tests/TestConfigurationObject.cs
+++
@@ -1,8 +0,0 @@
-namespace CommandLineArguments.Tests
-{
-	public class TestConfigurationObject
-	{
-		[CommandLineArgument(Aliases = new[] { "p", "prop" })]
-		public string Property { get; set; }
-	}
-}
